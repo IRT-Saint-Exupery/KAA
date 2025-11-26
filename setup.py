d7ed64be1b79661601bed4beec27c01903cc8b7d
Mohamed Chafik Bakey <mohamed-chafik.bakey@irt-saintexupery.com>
2025-11-26 16:08:21 +0100

    add PyPI packaging setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="kaa",
+    version="0.0.1",
+    url="https://github.com/IRT-Saint-Exupery/KAA",
+    author="Philippe Dejean",
+    author_email="philippe.dejean@irt-saintexupery.com",
+    description=("Kaa is a framework allowing to apply several methods and metrics" 
+                 "from several dedicated libraries on AI model to verify them."
+                ),
+    packages=find_packages(),
+    readme="README.md",
+    install_requires=[],
+    python_requires=">=3.8",
+    classifiers=["Programming Language :: Python :: 3.8",
+                 "License :: OSI Approved :: Apache Software License",
+                 "Operating System :: OS Independent"],
+)
