04cd48a5f9e6199d089969429e5be4a9c618e7ac
philippe.dejean <philippe.dejean@irt-saintexupery.com>
2025-12-17 11:08:43 +0000

    v25.12

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="kaa-xai",
-    version="0.0.1",
+    version="25.12",
     url="https://github.com/IRT-Saint-Exupery/KAA",
     author="Philippe Dejean",
     author_email="philippe.dejean@irt-saintexupery.com",
     description=("KAA is a framework allowing to apply several explainability methods and metrics"
                  "from several dedicated libraries on AI model to verify them."
                 ),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     readme="README.md",
     install_requires=[],
